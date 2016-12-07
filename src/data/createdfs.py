15bd46793fb826ae63db120c9d395fd71b7e8721
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2016-12-07 17:55:31 +0000

    organised data cleaning

--- src/data/createdfs.py
+++ src/data/createdfs.py
@@ -1,11 +1,9 @@
 from __future__ import division
 
-%load_ext autoreload
-%autoreload 2
-
 import helper_functions as hf
 
 raw_path = '../../data/raw/'
+processed_path = '../../processed/'
 fnames = ['debate_091016.txt','debate_191016.txt']
 
 with open(raw_path + fnames[0]) as f:
@@ -18,15 +16,19 @@ with open(raw_path + fnames[0]) as f:
     data = ([hf.try_extract(d,f) for f in fields] for d in data)
     data = (d for d in data if (d[-1] == 'en') and (d[0] is not None) and (d[0][:2] != 'RT'))
 
-    for chunk in hf.chunker(data,10000):
-        print(chunk[0][0])
+    for ind,chunk in enumerate(hf.chunker(data,10000)):
+        hf.make_df(chunk,ind,fnames[0])
 
 
-g = (i for i in range(1003))
+with open(raw_path + fnames[1]) as f:
 
-for grp in grouper(g,100):
-    print(sum(grp))
+    data = [line for line in f.readlines()[1:] if line != '\n']
 
-s = 'RT blah'
+    data = (eval(d) for d in data)
 
-s[:2]
+    fields = ['text','timestamp_ms','geo','place','lang']
+    data = ([hf.try_extract(d,f) for f in fields] for d in data)
+    data = (d for d in data if (d[-1] == 'en') and (d[0] is not None) and (d[0][:2] != 'RT'))
+
+    for ind,chunk in enumerate(hf.chunker(data,10000)):
+        hf.make_df(chunk,ind,fnames[1])
--- src/data/helper_functions.py
+++ src/data/helper_functions.py
@@ -1,10 +1,18 @@
 from itertools import islice
+from numpy import array, nan
+
+import pandas as pd
+from tempfile import NamedTemporaryFile
+import preprocessor as p
+import re
+import subprocess
 
 def try_extract(d,field):
     try:
         return d[field]
     except Exception:
         return None
+
 
 def chunker(iterable, n):
     iterable = iter(iterable)
@@ -19,4 +27,62 @@ def chunker(iterable, n):
                 group = []
         except StopIteration:
             yield group
-            brea
+            break
+
+def extract_name(d):
+    if d[3] != None:
+        return d[3]['full_name']
+    else:
+        return None
+
+def extract_centroid(d):
+    if d[3] != None:
+        return array(d[3]['bounding_box']['coordinates']).mean(axis=1)[0]
+    else:
+        return None
+
+def make_df(data_chunk,i,base_fname):
+    fname = '../../data/processed/{}_{}.csv'.format(base_fname,i)
+
+    coords = [extract_centroid(d) for d in data_chunk]
+    place_names = [extract_name(d) for d in data_chunk]
+    tweets = [d[0] for d in data_chunk]
+    dates = [pd.to_datetime(int(d[1])*1000000) for d in data_chunk]
+    data_chunk = {'coord':coords,'place_name':place_names,'tweet':tweets,'date':dates}
+
+    df = pd.DataFrame(data_chunk)
+    df = df.drop_duplicates(subset=['tweet'])
+    df = df.fillna(value=nan)
+
+    df['long'] = df.iloc[:,0].map(lambda x : x[0],na_action='ignore')
+    df['lat'] = df.iloc[:,0].map(lambda x : x[1],na_action='ignore')
+    df = df.drop('coord',axis=1)
+
+    regex = re.compile('[\.\t\,\:;\(\)\.\?\*\%\'\!\+\"]')
+    df.tweets = df.tweet.map(lambda x : x.encode('utf-8').strip())
+    df['cleaned_tweet'] = df.tweets.map(p.clean)
+    df['cleaned_tweet'] = df.cleaned_tweet.map(lambda x : regex.sub('',x).lower())
+
+    df = df.reset_index()
+    df = df.drop('index',axis=1)
+
+    t1 = NamedTemporaryFile()
+    t2 = NamedTemporaryFile()
+    t3 = NamedTemporaryFile()
+
+    df.tweet.to_csv(t1.name,encoding='utf-8',index=False)
+    df.cleaned_tweet.to_csv(t2.name,encoding='utf-8',index=False)
+
+    subprocess.call('curl --data-binary @{} "http://www.sentiment140.com/api/bulkClassify" > {}'.format(t1.name,t3.name),shell=True)
+    pols = pd.read_csv(t3.name,header=None)
+    df['polarity'] = pols[0]
+
+    subprocess.call('curl --data-binary @{} "http://www.sentiment140.com/api/bulkClassify" > {}'.format(t2.name,t3.name),shell=True)
+    pols = pd.read_csv(t3.name,header=None)
+    df['polarity_cl'] = pols[0]
+
+    t1.close()
+    t2.close()
+    t3.close()
+
+    df.to_csv(fname,encoding='utf-8')
